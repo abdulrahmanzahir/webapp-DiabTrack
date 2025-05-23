bb4c0a94c91bf8367bcebf7ebf0c5769015c698c
AbdulRahman Hosamaddin Ahmad Zahir <133116031+abdulrahmanzahir@users.noreply.github.com>
2025-05-23 15:29:52 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ app = FastAPI()
 # ───── Enable CORS ─────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://webapp-frontend-voa8.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
